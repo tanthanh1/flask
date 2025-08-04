65d85c183ca43744d344701a82efca44c6e9d123
Thanh <lygiathanh@gmail.com>
2025-08-04 10:02:26 +0700

    fix: use 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 HTML_FORM = '''
 <!doctype html>
 <title>Upload Excel</title>
-<h2>Tải file lên đi bà Lài</h2>
+<h2>Chưa có ai xài chùa mà có kết cục tốt !!!</h2>
 <form method=post enctype=multipart/form-data>
   <input type=file name=file>
   <input type=submit value="Bấm vô đây">
